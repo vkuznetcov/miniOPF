2433d7aff2f9dea190456242382deb9bff092ebf
slav <vkuznetcov48@gmail.com>
2021-11-30 21:20:13 +0400

    Initial commit

---
+++ src/main/java/com/netcracker/miniOPF/MiniOpfApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.miniOPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniOpfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netcracker/miniOPF/MiniOpfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.miniOPF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
