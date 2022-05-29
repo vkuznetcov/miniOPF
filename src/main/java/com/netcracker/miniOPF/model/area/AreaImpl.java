fb46b3d3c5f0c4c5ebdc2bcbaa6b04024ffd4680
slav <vkuznetcov48@gmail.com>
2022-05-29 18:03:48 +0400

    refactored AreaUtils and created tests for it

--- src/main/java/com/netcracker/miniOPF/model/area/AreaImpl.java
+++ src/main/java/com/netcracker/miniOPF/model/area/AreaImpl.java
@@ -2,15 +2,23 @@ package com.netcracker.miniOPF.model.area;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.util.Objects;
 
 
 @JsonTypeName("area")
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+@AllArgsConstructor
 public class AreaImpl implements Area
 {
     int id;
     String name;
     String description;
+
+    public AreaImpl(){}
 
     @Override
     public int getId()
@@ -46,5 +54,37 @@ public class AreaImpl implements Area
     public void setDescription(String description)
     {
         this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        AreaImpl area = (AreaImpl) o;
+        return id == area.id && Objects.equals(name, area.name) &&
+                Objects.equals(description, area.description);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "AreaImpl{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
