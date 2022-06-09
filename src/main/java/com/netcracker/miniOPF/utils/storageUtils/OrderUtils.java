27c3b992b4f4730ded859e096ef693480f3e6fa8
slav <vkuznetcov48@gmail.com>
2022-06-09 20:28:30 +0400

    refactored OrderUtils

--- src/main/java/com/netcracker/miniOPF/utils/storageUtils/OrderUtils.java
+++ src/main/java/com/netcracker/miniOPF/utils/storageUtils/OrderUtils.java
@@ -3,167 +3,106 @@ package com.netcracker.miniOPF.utils.storageUtils;
 import com.netcracker.miniOPF.model.order.Order;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 
 @Component
 public class OrderUtils
 {
 
-    public List<Order> sortOrdersByID(List<Order> values)
+    public List<Order> sortOrdersByID(List<Order> values, boolean reversed)
     {
+        ListUtils.checkListIsEmptyOrNull(values);
+
+        if (reversed)
+        {
+            return values.stream().sorted((o1, o2) -> o2.getId() - o1.getId()).toList();
+        }
         return values.stream().sorted(Comparator.comparingInt(Order::getId)).toList();
     }
 
-    public List<Order> sortOrdersByIDReversed(List<Order> values)
+    public List<Order> sortOrdersByAdminID(List<Order> values, boolean reversed)
     {
-        return values.stream().sorted((o1, o2) -> o2.getId() - o1.getId()).toList();
-    }
+        ListUtils.checkListIsEmptyOrNull(values);
 
-    public List<Order> sortOrdersByAdminID(List<Order> values)
-    {
-//        return values.stream().sorted(Comparator.comparing(o -> o.getAdmin().getLogin())).toList();
-        // TODO можно заменить на лямбда выражение
-        return values.stream().sorted(new Comparator<Order>()
+        if (reversed)
         {
-            @Override
-            public int compare(Order o1, Order o2)
-            {
-                return o1.getAdmin().getId() - o2.getAdmin().getId();
-            }
-        }).toList();
+            return values.stream().sorted((o1, o2) -> o2.getAdmin().getId() - o1.getAdmin().getId()).toList();
+        }
+        return values.stream().sorted((o1, o2) -> o1.getAdmin().getId() - o2.getAdmin().getId()).toList();
 
     }
 
-    public List<Order> sortOrdersByAdminIDReversed(List<Order> values)
+    public List<Order> sortOrdersByServiceID(List<Order> values, boolean reversed)
     {
-        return values.stream().sorted(new Comparator<Order>()
+        ListUtils.checkListIsEmptyOrNull(values);
+
+        if (reversed)
         {
-            @Override
-            public int compare(Order o1, Order o2)
-            {
-                return o2.getAdmin().getId() - o1.getAdmin().getId();
-            }
-        }).toList();
+            return values.stream().sorted((o1, o2) -> o2.getService().getId() - o1.getService().getId()).toList();
+        }
+        return values.stream().sorted((o1, o2) -> o1.getService().getId() - o2.getService().getId()).toList();
     }
 
-    public List<Order> sortOrdersByServiceID(List<Order> values)
+    public List<Order> sortOrdersByStatus(List<Order> values, boolean reversed)
     {
-        return values.stream().sorted(new Comparator<Order>()
+        ListUtils.checkListIsEmptyOrNull(values);
+
+        if (reversed)
         {
-            @Override
-            public int compare(Order o1, Order o2)
-            {
-                return o1.getService().getId() - o2.getService().getId();
-            }
-        }).toList();
+            return values.stream()
+                         .sorted((o1, o2) -> o2.getStatus().toString().compareTo(o1.getStatus().toString()))
+                         .toList();
+
+        }
+        return values.stream()
+                     .sorted((o1, o2) -> o1.getStatus().toString().compareTo(o2.getStatus().toString()))
+                     .toList();
     }
 
-    public List<Order> sortOrdersByServiceIDReversed(List<Order> values)
+    public List<Order> sortOrdersByAction(List<Order> values, boolean reversed)
     {
-        return values.stream().sorted(new Comparator<Order>()
+        ListUtils.checkListIsEmptyOrNull(values);
+
+        if (reversed)
         {
-            @Override
-            public int compare(Order o1, Order o2)
-            {
-                return o2.getService().getId() - o1.getService().getId();
-            }
-        }).toList();
-    }
+            return values.stream()
+                         .sorted((o1, o2) -> o2.getAction().toString().compareTo(o1.getAction().toString()))
+                         .toList();
 
-    public List<Order> sortOrdersByStatus(List<Order> values)
-    {
-        return values.stream().sorted(Comparator.comparing(Order::getStatus)).toList();
-    }
-
-    public List<Order> sortOrdersByStatusReversed(List<Order> values)
-    {
-        return values.stream().sorted((o1, o2) -> o2.getStatus().compareTo(o1.getStatus())).toList();
-    }
-
-    public List<Order> sortOrdersByAction(List<Order> values)
-    {
-        return values.stream().sorted(Comparator.comparing(Order::getAction)).toList();
-    }
-
-    public List<Order> sortOrdersByActionReversed(List<Order> values)
-    {
-        return values.stream().sorted((o1, o2) -> o2.getAction().compareTo(o1.getAction())).toList();
+        }
+        return values.stream()
+                     .sorted((o1, o2) -> o1.getAction().toString().compareTo(o2.getAction().toString()))
+                     .toList();
     }
 
     public Order searchOrderByID(List<Order> values, int id)
     {
-//        List<Order> list = new ArrayList<>();
-//        for (Order cur : values)
-//        {
-//            if (cur.getID() == id)
-//            {
-//                list.add(cur);
-//            }
-//        }
-//        return list;
-
-        for (Order cur : values)
-        {
-            if (cur.getId() == id)
-            {
-                return cur;
-            }
-        }
-        return null;
+        ListUtils.checkListIsEmptyOrNull(values);
+        return values.stream().filter(i -> i.getId() == id).findFirst().orElse(null);
     }
 
     public List<Order> searchOrdersByAdminID(List<Order> values, int id)
     {
-        List<Order> list = new ArrayList<>();
-        for (Order cur : values)
-        {
-            if (cur.getAdmin().getId() == id)
-            {
-                list.add(cur);
-            }
-        }
-        return list;
+        ListUtils.checkListIsEmptyOrNull(values);
+        return values.stream().filter(i -> i.getAdmin().getId() == id).toList();
     }
 
     public List<Order> searchOrdersByServiceID(List<Order> values, int id)
     {
-        List<Order> list = new ArrayList<>();
-        for (Order cur : values)
-        {
-            if (cur.getService().getId() == id)
-            {
-                list.add(cur);
-            }
-        }
-        return list;
+        ListUtils.checkListIsEmptyOrNull(values);
+        return values.stream().filter(i -> i.getService().getId() == id).toList();
     }
 
     public List<Order> searchOrdersByStatus(List<Order> values, String status)
     {
-        List<Order> list = new ArrayList<>();
-        for (Order cur : values)
-        {
-            if (cur.getStatus().toString().equals(status.toUpperCase(Locale.ROOT)))
-            {
-                list.add(cur);
-            }
-        }
-        return list;
+        ListUtils.checkListIsEmptyOrNull(values);
+        return values.stream().filter(i -> i.getStatus().toString().equals(status)).toList();
     }
 
     public List<Order> searchOrdersByAction(List<Order> values, String action)
     {
-        List<Order> list = new ArrayList<>();
-        for (Order cur : values)
-        {
-            if (cur.getAction().toString().equals(action.toUpperCase(Locale.ROOT)))
-            {
-                list.add(cur);
-            }
-        }
-        return list;
+        ListUtils.checkListIsEmptyOrNull(values);
+        return values.stream().filter(i -> i.getAction().toString().equals(action)).toList();
     }
 }
